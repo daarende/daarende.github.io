53e6a666a94b839a1f5d0296f67f994679d7fbd3
Daan Renders <daan@fluves.com>
2018-12-27 23:48:10 +0100

    adding scale and fetch jsonfeatures

--- scripts/map.js
+++ scripts/map.js
@@ -1,23 +1,35 @@
-var mymap = L.map('results').setView([50.937430, 4.040934], 13);
+var URL = 'http://api.luftdaten.info/static/v2/data.json';
+var mapcenter = [50.937430, 4.040934]
+
+var map = L.map('results').setView(mapcenter, 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 15,
     minZoom: 12,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZGFhcmVuZGUiLCJhIjoiY2pweDZ1aWJnMDEyMDQxbnhjeDE5bnNqbyJ9.lObt90eRQP4teg0CQfzOgA'
-}).addTo(mymap);
+}).addTo(map);
 
-// let hexLayer = new leaflet.HexbinLayer(options).addTo(map)
+var hash = new L.Hash(map)
 
-//api.getAllSensors().then((cells) => {
-//	hexLayer.data(cells)
-//})
+L.control.scale(options = (imperial = false)).addTo(map);
 
-var hash = new L.Hash(mymap)
+var jsonFeatures = fetch(URL).then(function(result){
+		return result.json();
+});
 
-URL = 'http://api.luftdaten.info/static/v2/data.json';
-let api = {
-	fetchNow() {
-		return fetch(URL).then((response) => response.json())
-}}
-	
+
+api.forEach(function(point){
+   var lat = point.latitud;
+   var lon = point.longitud;
+   var feature = {type: 'Feature',
+       properties: point,
+       geometry: {
+           type: 'Point',
+           coordinates: [lon,lat]
+       }
+  };
+   jsonFeatures.push(feature);
+});
+var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
+L.geoJson(geoJson).addTo(map);
