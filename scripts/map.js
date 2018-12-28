b8403905ff6310176a8b69691229c0f8203d4580
Daan Renders <daan@fluves.com>
2018-12-28 01:18:45 +0100

    adding filtering of json

--- scripts/map.js
+++ scripts/map.js
@@ -19,7 +19,21 @@ var jsonFeatures = []
 fetch(URL)
 	.then((response) => response.json())
 	.then(function(data) {
-		Object.values(data).forEach((sensor) => {
+		// filter data
+		var filtered = Object.values(data).filter((sensor) =>
+					sensor.location.latitude != null &&
+					sensor.location.longitude != null && (
+					// (sensor.sensor.sensor_type.name == "PPD42NS" && sensor.sensordatavalues.length >= 6) ||
+					(sensor.sensor.sensor_type.name == "HPM" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == "PMS1003" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == "PMS3003" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == "PMS5003" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == "PMS6003" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == "PMS7003" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == "SDS021" && sensor.sensordatavalues.length >= 2) ||
+					(sensor.sensor.sensor_type.name == 'SDS011' && sensor.sensordatavalues.length >= 2)))
+		// convert data to geoJson
+		filtered.forEach((sensor) => {
 			var lat = sensor.latitud;
 			var lon = sensor.longitud;
 			var feature = {type: 'Feature',
